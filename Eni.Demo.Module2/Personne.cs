d536ac525199a31a810bc2f212a54a3c219ba109
Teo BRYER <teo.bryer2020@campus-eni.fr>
2022-05-16 11:15:13 +0200

    Ajoutez des fichiers projet.

---
+++ Eni.Demo.Module2/Personne.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Eni.Demo.Module2
+{
+    public class Personne
+
+        // atributs privés
+    {
+    }
+}
