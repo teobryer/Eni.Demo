d241b2d49372a8f5a26236bda82ec11df141c76c
tbryer2020 <tbryer2020@1SE19-2XXTT72>
2022-05-16 16:41:10 +0200

    Indentation

--- Tp_Heritage/BO/Triangle.cs
+++ Tp_Heritage/BO/Triangle.cs
@@ -17,39 +17,34 @@ namespace Tp_Heritage.BO
 
 
 
-            var cote = new Func<int,int, int, String>((a,b,c) =>
+            var cote = new Func<int, int, int, String>((a, b, c) =>
+             {
+
+                 return $"Triangle de coté A = {a}, B = {b}, C = {c} \n";
+             });
+
+
+            var perimetre = new Func<int, int, int, String>((a, b, c) =>
             {
 
-                return $"Triangle de coté A = {a}, B = {b}, C = {c} \n";
-            });
-
-
-
-
-        
-
-
-            var perimetre = new Func<int, int, int, String>((a,b,c) =>
-            {
-
-                return $"Perimetre  = {a+b+c } \n";
+                return $"Perimetre  = {a + b + c } \n";
             });
 
             var perimetreDivide2 = new Func<int, int, int, double>((a, b, c) =>
             {
 
-                return (a + b + c) /2.0 ;
+                return (a + b + c) / 2.0;
             });
 
 
             var aire = new Func<int, int, int, double, String>((a, b, c, per) =>
             {
 
-                return $"Aire = {Math.Sqrt(per * (per - a)* (per - b) * (per - c))} \n";
+                return $"Aire = {Math.Sqrt(per * (per - a) * (per - b) * (per - c))} \n";
             });
 
 
-            return cote(A,B,C) + aire(this.A, this.B, this.C,(perimetreDivide2(A,B,C)) ) + perimetre(A,B,C);
+            return cote(A, B, C) + aire(this.A, this.B, this.C, (perimetreDivide2(A, B, C))) + perimetre(A, B, C);
         }
     }
 }
